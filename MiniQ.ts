1927eb5ae62a077caacf86cf09333e0519225aff
Kwright02 <kyle.wright02@gmail.com>
2022-07-07 03:09:17 -0500

    Starting for server

--- MiniQ.ts
+++ MiniQ.ts
@@ -1,9 +1,29 @@
 import Router from "./src/components/Router.ts";
 import ComponentMapper from "./src/framework/ComponentMapper.ts";
+import { Server } from "https://deno.land/std@0.147.0/http/server.ts";
 
 export default class MiniQ {
 
+    static readonly DEFAULT_PORT = 3000;
+
     static mapper: ComponentMapper = new ComponentMapper();
     static router: Router = new Router();
+    static server: Server = new Server({ port: this.DEFAULT_PORT, handler: MiniQ.defaultRequestHandler });
+
+    private static defaultRequestHandler(req: Request) {
+        console.log(req);
+
+        return new Response("OK", { status: 200 });
+    }
+
+    static async start(port?: number, callback?: Function){
+        if(port) MiniQ.server = new Server({ port: port, handler: MiniQ.defaultRequestHandler });
+        if(callback) callback(port);
+        await this.server.listenAndServe();
+    }
+
+    static stop(){
+        this.server.close();
+    }
 
 }
